4b1737141044d1e654d463fda129f832d5200406
unknown <malanchuksam@gmail.com>
2020-03-11 19:42:55 -0400

    created a basic queue based on the Double linked list class

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -7,13 +7,37 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
+
+    def __str__(self):
+        if self.storage.head is None and self.storage.tail is None:
+            return "empty"
+        curr_node = self.storage.head
+        output = ""
+        output += f'({curr_node.value})'
+        while curr_node.next is not None:
+            curr_node = curr_node.next
+            output += f' <-> ({curr_node.value})'
+        return output
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_head(value)
 
     def dequeue(self):
-        pass
+        return self.storage.remove_from_tail()
 
     def len(self):
-        pass
+        return self.size
+
+myQ = Queue()
+
+myQ.enqueue(4)
+myQ.enqueue(2)
+myQ.enqueue(1)
+myQ.enqueue(3)
+
+print(myQ)
+
+removeVal = myQ.dequeue()
+print(f'Removed: {removeVal}')
+print(myQ)
